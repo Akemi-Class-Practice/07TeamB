1cf0c41b85fa82e595b9a45352629ab407e8a0c2
so4986 <103545825+so4986@users.noreply.github.com>
2023-09-01 14:10:37 +0900

    Add files via upload

---
+++ resources/templates/user.js
@@ -0,0 +1,31 @@
+document.getElementById("user_register_form").addEventListener("submit",function(event){
+    var nameInput = document.getElementsByName("user_name")[0];
+    var emailInput = document.getElementsByName("user_email")[0];
+    var passwordInput = document.getElementById("password");
+    var confirmPasswordInput = document.getElementById("confirmPassword");
+    var errorMessage = document.getElementById("error_message");
+
+    if(nameInput.value.trim() === "" || emailInput.value.trim() ==="" || passwordInput.value === "" || confirmPasswordInput.value === "" || !document.querySelector(".check_box").checked){
+        event.preventDefault();
+        errorMessage.textContent = "未入力の項目があります、利用者規則に同意していません。";
+    }else if(passwordInput.value !== confirmPasswordInput.value){
+        event.preventDefault();
+        errorMessage.textContent = "パスワードが一致しません。";
+    }else{
+        errorMessage.textContent = "";
+    }
+});
+
+
+document.getElementById("user_login_form").addEventListener("submit",function(event){
+    var emailInput = document.getElementsByName("user_email")[0];
+    var passwordInput = document.getElementsByName("user_password")[0];
+    var errorMessage = document.getElementById("error_message");
+    
+    if(emailInput.value.trim() === "" || passwordInput.value === "" ){
+        event.preventDefault();
+        errorMessage.textContent = "間違いがあります。";
+    }else{
+        errorMessage.textContent = "";
+    }
+});

69a502e646edd2ab05c20475f212a909cfaa957a
Jutta197 <yangzigui99@gmail.com>
2023-09-06 15:16:15 +0800

    teamb new

---
+++ Beeeee/src/main/java/com/example/entity/AdminInfo.java
@@ -0,0 +1,30 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Table(name = "admin_info")
+public class AdminInfo extends Account {
+
+//    @Column(name = "phone")
+//    private String phone;
+//
+//    public String getPhone() {
+//        return phone;
+//    }
+//
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
+    
+//    @Column(name = "email")
+//    private String email;
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setPhone(String email) {
+//        this.email = email;
+//    }
+}

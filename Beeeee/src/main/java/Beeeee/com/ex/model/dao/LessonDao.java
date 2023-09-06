750b6956e99065dc4294da7f247ca7d84e385375
Jutta <50023070+Jutta197@users.noreply.github.com>
2023-09-06 15:01:58 +0800

    Delete Beeeee directory

--- Beeeee/src/main/java/Beeeee/com/ex/model/dao/LessonDao.java
+++
@@ -1,9 +0,0 @@
-package Beeeee.com.ex.model.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Beeeee.com.ex.model.entity.LessonEntity;
-
-public interface LessonDao extends JpaRepository<LessonEntity, Long> {
-
-}
